7f176b6b3ef6aafe69b6d0d27e44f1e3a0fefdc6
Thefoovault <marta.ag.ma@gmail.com>
2021-12-22 22:09:56 +0100

    Added Product in the Shared BC to handle all discount logic

---
+++ src/Shared/Domain/Discount/Product/ProductPrice.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shared\Domain\Discount\Product;
+
+use Shared\Domain\ValueObject\IntegerValueObject;
+
+final class ProductPrice extends IntegerValueObject
+{
+}
